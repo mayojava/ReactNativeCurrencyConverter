34084911c637f0a6d8304fe2f92867289289db3b
Mayowa Adegeye <mayowa.adegeye@agoda.com>
2018-02-10 14:37:57 +0700

    Completed home page design

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,23 +1,102 @@
 import React from 'react';
-import { View, Image, Text, ImageBackground } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  ImageBackground,
+  Keyboard,
+  Animated,
+  Platform,
+} from 'react-native';
 
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      style={styles.containerImage}
-      source={require('./images/background.png')}
-      resizeMode="contain"
-    >
-      <Image
-        source={require('./images/logo.png')}
-        resizeMode="contain"
-        style={styles.image}
-      />
-    </ImageBackground>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+const ANIMATION_DURATION = 250;
+
+class Logo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+  }
+
+  componentDidMount() {
+    let showListener = 'keyboardWillShow';
+    let hideListener = 'keyboardWillHide';
+
+    if (Platform.OS === 'android') {
+      showListener = 'keyboardDidShow';
+      hideListener = 'keyboardDidHide';
+    }
+
+    this.keyboardShowListener = Keyboard.addListener(
+      showListener,
+      this.keyBoardShow,
+    );
+    this.keyboardHideListener = Keyboard.addListener(
+      hideListener,
+      this.keyBoardHide,
+    );
+  }
+
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+  }
+
+  keyBoardShow = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$smallContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$smallImageSize,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  keyBoardHide = () => {
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$largeContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$largeImageSize,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  render() {
+    const containerImageStyle = [
+      styles.containerImage,
+      { width: this.containerImageWidth, height: this.containerImageWidth },
+    ];
+
+    const imageStyle = [styles.image, { width: this.imageWidth }];
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.containerImage}
+          source={require('./images/background.png')}
+          resizeMode="contain"
+        >
+          <Animated.Image
+            source={require('./images/logo.png')}
+            resizeMode="contain"
+            style={imageStyle}
+          />
+        </ImageBackground>
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+}
 
 export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -9,6 +9,7 @@ EStyleSheet.build({
   $border: '#E2E2E2',
   $inputText: '#797979',
   $lightGray: '#F0F0F0',
+  $darkText: '#343434',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
